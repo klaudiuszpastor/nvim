ff08bd9600f5a822ca409b9a547f853fb677e5b0
Klaudiusz Pastor <pastor.klo12@gmail.com>
2024-09-18 11:35:24 +0200

    toggleterm keymap update

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,8 +2,10 @@ return {
 	"akinsho/toggleterm.nvim",
 
 	config = function()
-		require("toggleterm").setup({
-			shade_terminals = false,
-		})
+	require("toggleterm").setup({
+		shade_terminals = false,
+	})
+		vim.keymap.set("n","<leader>tr",":ToggleTerm<CR>",{})
+		vim.keymap.set('t', '<leader>tr', "<C-\\><C-n>",{})
 	end,
 }
