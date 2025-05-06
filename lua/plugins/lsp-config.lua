74a16c0cf0c33381f5739ddd10d068e2165d06b1
Klaudiusz Pastor <pastor.klo12@gmail.com>
2025-05-06 09:10:37 +0200

    lsp config/lualine delation

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,65 +1,58 @@
 return {
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
   },
   {
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
           "clangd",
-	    "mesonlsp",
-	    "cmake",
-	    "ltex",
-	    "marksman",
-	    "pyre",
+          "mesonlsp",
+          "cmake",
+          "ltex",
+          "marksman",
+          "pyre",
+          "bashls",    -- Bash Language Server (lspconfig name) :contentReference[oaicite:0]{index=0}
         }
       })
-  end
+    end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
+      local lspconfig   = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      lspconfig.lua_ls.setup({
-        				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
 
+      -- existing servers
+      lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } }, capabilities = capabilities })
+      lspconfig.clangd.setup({                  capabilities = capabilities })
+      lspconfig.mesonlsp.setup({                capabilities = capabilities })
+      lspconfig.cmake.setup({                   capabilities = capabilities })
+      lspconfig.ltex.setup({                    capabilities = capabilities })
+      lspconfig.marksman.setup({                capabilities = capabilities })
+      lspconfig.pyre.setup({                    capabilities = capabilities })
+
+      -- Bash LSP
+      lspconfig.bashls.setup({
         capabilities = capabilities,
       })
-      lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-	lspconfig.mesonlsp.setup({
-				capabilities = capabilities,
-			})
-	lspconfig.cmake.setup({
-				capabilities = capabilities,
-			})
-	lspconfig.ltex.setup({
-				capabilities = capabilities,
-			})
-	lspconfig.marksman.setup({
-				capabilities = capabilities,
-			})
-	lspconfig.pyre.setup({
-				capabilities = capabilities,
-			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      -- Perl LSP (PLS)
+      lspconfig.perlpls.setup({
+        capabilities = capabilities,
+      })
+
+      -- common keymaps
+      vim.keymap.set("n", "K",           vim.lsp.buf.hover,        {})
+      vim.keymap.set("n", "<leader>gd",  vim.lsp.buf.definition,   {})
+      vim.keymap.set("n", "<leader>gr",  vim.lsp.buf.references,   {})
+      vim.keymap.set("n", "<leader>ca",  vim.lsp.buf.code_action,  {})
     end
   }
 }
+
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,16 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-
-	config = function()
-		require("lualine").setup({
-			options = {
-				--theme = 'vscode' 
-				--theme = 'gruvbox_dark'
-				theme = "auto"
-			}
-		})
-		-- require("transparent").clear_prefix("lualine")
-	end,
+--	"nvim-lualine/lualine.nvim",
+--	dependencies = { "nvim-tree/nvim-web-devicons" },
+--
+--	config = function()
+--		require("lualine").setup({
+--			options = {
+--				--theme = 'vscode' 
+--				--theme = 'gruvbox_dark'
+--				theme = "auto"
+--			}
+--		})
+--		-- require("transparent").clear_prefix("lualine")
+--	end,
 }
 
