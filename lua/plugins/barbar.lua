bc8149e0ce72a1f3e7c63e882f326612202dd8c7
Klaudiusz Pastor <pastor.klo12@gmail.com>
2025-05-09 21:02:50 +0200

    barbar and lualine style update

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,47 +7,40 @@ return {
 			auto_hide = false,
 			clickable = true,
 			tabpages = true,
+			highlight_alternate = false,
+			higlight_inactive_file_icons = false,
+			highlight_visible = false,
 			maximum_padding = 1,
 			minimum_padding = 1,
-
-			icons = {
-				-- turn off the index number
-				buffer_index = false,
-
-				-- disable filetype icons
-				filetype = {
-					enabled = false,
-				},
-
-				-- disable modified indicator
-				modified = {
-					enabled = false,
-				},
-
-				-- disable the close button
-				button = false,
-			},
+      icons = {
+        buffer_index = false,
+        filetype = { enabled = false },
+        modified = { enabled = false },
+        button = false,
+				separator_at_end = false,
+      },
 		})
 		vim.keymap.set("n", "<tab>", ":BufferNext<CR>")
 		vim.keymap.set("n", "<S-tab>", ":BufferPrevious<CR>")
 		vim.keymap.set("n", "<leader>x", ":BufferClose<CR>")
 
-		-- Zakładki
-		vim.api.nvim_set_hl(0, "BufferCurrent",        { fg = "#ffffff", bg = "none"})
-		vim.api.nvim_set_hl(0, "BufferCurrentMod",     { fg = "#ffff5f", bg = "none"})
-
-		-- Nieaktywne zakładki
-		vim.api.nvim_set_hl(0, "BufferInactive",       { fg = "#aaaaaa", bg = "#1c1c1c" })
-		vim.api.nvim_set_hl(0, "BufferInactiveMod",    { fg = "#ffff5f", bg = "#1c1c1c" })
-		vim.api.nvim_set_hl(0, "BufferVisible",        { fg = "#ffffff", bg = "#1c1c1c" })
-
-		-- Pasek tła
-		vim.api.nvim_set_hl(0, "BufferTabpageFill",    { bg = "none" })
-		vim.api.nvim_set_hl(0, "BufferOffset",         { bg = "none" })
-
-		-- Nadpisanie ostrzeżeń 
-		vim.api.nvim_set_hl(0, "BufferWarning",        { fg = "#ffff00", bg = "#1c1c1c" })
-		vim.api.nvim_set_hl(0, "BufferWarningMod",     { fg = "#ffff00", bg = "#1c1c1c", bold = true })
+    -- Uniform white background and black text for all barbar components
+    local style = { fg = "#FFFFFF", bg = "#000000" }
+    -- Current buffer
+    vim.api.nvim_set_hl(0, "BufferCurrent",      style)
+    vim.api.nvim_set_hl(0, "BufferCurrentMod",   style)
+    vim.api.nvim_set_hl(0, "BufferCurrentIndex", style)
+    -- Visible buffers (not current)
+    vim.api.nvim_set_hl(0, "BufferVisible",      style)
+    vim.api.nvim_set_hl(0, "BufferVisibleMod",   style)
+    vim.api.nvim_set_hl(0, "BufferVisibleIndex", style)
+    -- Inactive buffers
+    vim.api.nvim_set_hl(0, "BufferInactive",      style)
+    vim.api.nvim_set_hl(0, "BufferInactiveMod",   style)
+    vim.api.nvim_set_hl(0, "BufferInactiveIndex", style)
+    -- Separators between buffers
+    vim.api.nvim_set_hl(0, "BufferSeparator",    style)
+   
 		-- require("transparent").clear_prefix("barbar")
 	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,64 @@
+-- ~/.config/nvim/lua/plugins/lualine.lua
 return {
---	"nvim-lualine/lualine.nvim",
---	dependencies = { "nvim-tree/nvim-web-devicons" },
---
---	config = function()
---		require("lualine").setup({
---			options = {
---				--theme = 'vscode' 
---				--theme = 'gruvbox_dark'
---				theme = "auto"
---			}
---		})
---		-- require("transparent").clear_prefix("lualine")
---	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled        = false,
+        component_separators = { left = '', right = '' },
+        section_separators   = { left = '', right = '' },
+        theme = {
+          -- black text on white background
+          normal   = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+          insert   = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+          visual   = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+          replace  = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+          command  = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+          inactive = { a = { fg = '#000000', bg = '#FFFFFF' }, b = { fg = '#000000', bg = '#FFFFFF' }, c = { fg = '#000000', bg = '#FFFFFF' } },
+        },
+        disabled_filetypes = {},
+      },
+      sections = {
+        lualine_a = { 'mode' },       -- show only the mode
+        lualine_b = {
+          {
+            'diff',
+            colored = true,
+            diff_color = {
+              added    = { fg = '#000000' },
+              modified = { fg = '#000000' },
+              removed  = { fg = '#000000' },
+            },
+          },
+          {
+            'diagnostics',
+            sources = { 'nvim_lsp' },
+            sections = { 'error', 'warn', 'info', 'hint' },
+            colored = true,
+            color_error = { fg = '#000000' },
+            color_warn  = { fg = '#000000' },
+            color_info  = { fg = '#000000' },
+            color_hint  = { fg = '#000000' },
+          },
+        },
+       lualine_c = {
+          { 'filename', path = 1 }
+        },
+        lualine_x = {},
+        lualine_y = {'tabs'},
+        lualine_z = {'searchcount','location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    }
+  end,
 }
 
